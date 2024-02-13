1365ec79052dc93dc5d448e5d8f1be970b5043e9
Niket Yende <niket6@gmail.com>
2024-02-13 22:47:43 +0530

    Add function documentation for Voting contract

--- src/Voting.sol
+++ src/Voting.sol
@@ -3,6 +3,11 @@ pragma solidity 0.8.24;
 
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
+/**
+ * @title Voting
+ * @dev A smart contract for conducting voting on proposed items.
+ *      Each item can be proposed, and users can vote for their preferred items.
+ */
 contract Voting is ReentrancyGuard {
     uint256 public itemId;
     uint64 public immutable itemLimit;
@@ -20,10 +25,19 @@ contract Voting is ReentrancyGuard {
 
     event VoteSubmitted(address _voter);
 
+    /**
+     * @dev Constructor to initialize the contract with the specified item limit.
+     * @param _itemLimit The maximum number of items that can be proposed.
+     */
     constructor(uint64 _itemLimit) {
         itemLimit = _itemLimit;
     }
 
+    /**
+     * @dev Allows a user to propose an item for voting.
+     * @param _item The name of the item to propose.
+     * @return _itemId The unique ID of the proposed item.
+     */
     function proposeItem(string calldata _item) external returns (uint256) {
         require(bytes(_item).length > 0, 'Item must be present');
         require(!itemExist[_item], 'Duplicate item added');
@@ -36,6 +50,10 @@ contract Voting is ReentrancyGuard {
         return _itemId;
     }
 
+    /**
+     * @dev Allows a user to vote for a specific item.
+     * @param _itemId The ID of the item to vote for.
+     */
     function voteForItem(uint256 _itemId) external nonReentrant {
         // Check required conditions
         require(_itemId > 0 && _itemId <= items.length, 'Provide valid Item id');
@@ -49,6 +67,11 @@ contract Voting is ReentrancyGuard {
         votes[_itemId]++; 
     }
 
+    /**
+     * @dev Retrieves the winner of the voting.
+     * @return id The ID of the winning item.
+     * @return name The name of the winning item.
+     */
     function getWinner() public view returns (uint, string memory) {
         uint highestVote = 0;
         // Local caching
